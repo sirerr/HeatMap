9d096a052719bef4626b4220c6ce7b2ad29e1730
sirerr <sirerr@gmail.com>
2016-11-02 18:13:55 -0400

    getting new parts in there

--- Unity/HeatMap App/Assets/scripts/appman.cs
+++ Unity/HeatMap App/Assets/scripts/appman.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class appman : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+
+	//gameobjects in lockscreen
+	public GameObject lockscreen;
+	public GameObject unlockbutton;
+
+	//appbutton
+	public GameObject appbutton;
+	public GameObject homescreen;
+
+	//background of app
+	public GameObject mainbackground;
+	//firstscreen
+	public GameObject firstscreenObj;
+
+
+	public void unlockphone()
+	{
+		lockscreen.SetActive(false);
+	//	print("unlockedphone");
+		unlockbutton.SetActive(false);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void startApp()
+	{
+		appbutton.SetActive(false);
+		homescreen.SetActive(false);
+		mainbackground.SetActive(true);
+		firstscreenObj.SetActive(true);
 	}
+
+	public void backtohome()
+	{
+		appbutton.SetActive(true);
+		homescreen.SetActive(true);
+
+	}
+
+	public void  logintoApp()
+	{
+		print("loggin into the app");
+
+	}
+
+
 }
---
+++ Unity/HeatMap App/Assets/scripts/appstartingscreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class appstartingscreen : MonoBehaviour {
+
+	//logo
+	public GameObject logoObj;
+
+	public GameObject otherobj0;
+	public GameObject otherobj1;
+	public GameObject otherobj2;
+	public GameObject otherobj3;
+	public GameObject otherobj4;
+
+	public void OnEnable()
+	{
+		logoObj.SetActive(true);
+
+		StartCoroutine(doMore());
+	}
+
+	IEnumerator doMore()
+	{
+		yield return new WaitForSeconds(2f);
+		otherobj0.SetActive(true);
+		otherobj1.SetActive(true);
+		otherobj2.SetActive(true);
+		otherobj3.SetActive(true);
+		otherobj4.SetActive(true);
+
+	}
+}
