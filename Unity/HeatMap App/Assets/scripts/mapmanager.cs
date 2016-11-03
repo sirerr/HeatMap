070c42af6118f1043bebbd53da66ea582ca00ea2
sirerr <sirerr@gmail.com>
2016-11-03 16:01:01 -0400

    quick push

---
+++ Unity/HeatMap App/Assets/scripts/mapmanager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mapmanager : MonoBehaviour {
+
+	public Button openslider;
+
+	public Button bar;
+	public Button resturant;
+	public Button coffee;
+	public Button grocery;
+	public Button clubs;
+	public Button publicareas;
+
+
+	//barobjects
+	public GameObject barBack;
+	public GameObject barHeat;
+
+	//resturant
+	public GameObject restBack;
+	public GameObject restHeat;
+
+	//barobjects
+	public GameObject coffeeBack;
+	public GameObject coffeeHeat;
+
+	//resturant
+	public GameObject groceryBack;
+	public GameObject groceryHeat;
+
+	//barobjects
+	public GameObject clubBack;
+	public GameObject clubHeat;
+
+	//resturant
+	public GameObject publicBack;
+	public GameObject publicHeat;
+
+
+
+	public void barAction()
+	{
+		if(!barBack.activeSelf)
+		{
+			barBack.SetActive(true);
+		//	print("turn on");
+		}
+		else
+		{
+
+			barBack.SetActive(false);
+		//	print("turn off");
+		}
+
+	}
+	public void restAction()
+	{
+
+
+	}
+
+
+
+
+}
