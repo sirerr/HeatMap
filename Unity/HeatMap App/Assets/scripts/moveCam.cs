62b252579192d57775525700585e388d6a5085bc
sirerr <sirerr@gmail.com>
2016-11-06 23:05:58 -0500

    got movement working

---
+++ Unity/HeatMap App/Assets/scripts/moveCam.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCam : MonoBehaviour {
+
+	public Rigidbody movecam;
+
+	public float pushfloat =5;
+
+	public Transform centerpoint;
+	public Camera topcam;
+
+	public Vector3 dir;
+	public appman appmanref;
+
+	void Start()
+	{
+		topcam = GetComponent<Camera>();
+
+	}
+
+	public void Update()
+	{
+		bool mouseclick = Input.GetMouseButtonDown(0);
+		Vector3 mousepos = topcam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,topcam.nearClipPlane));
+		print(mousepos);
+
+		if(mouseclick && appmanref.MapObj.activeSelf)
+		{
+			dir = mousepos - centerpoint.position;
+			movecam.AddForce(dir *pushfloat *Time.deltaTime);
+		}
+
+	
+
+
+	}
+
+}
